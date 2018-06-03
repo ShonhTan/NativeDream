6b4a9880dfeef25d5069d22358ee16783a5acee9
ShonhTan <thanh-son-vincent.pham@hetic.net>
2018-06-04 00:27:25 +0200

    add result + update sequence

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 var sequence = []
 var TT = 10
+
 function reinit(){
     sequence=[]
+    sequenceRecord = []
     sequenceCopy.forEach(element =>
         sequence.push(element)
     )
+    game.result ={
+        perfect:0,
+        cool:0,
+        bad:0,
+        miss:0
+    }
+    gameElement.result.innerHTML="";    
 }
 ////
 
+//element de la page
 var gameElement = {
     player: document.querySelector('.gamePlayer'),
     colonne: document.querySelectorAll(".colonne_sequence"),
     letter: document.querySelectorAll(".commands_letter"),
     container: document.querySelector('.container'),
-    button: document.querySelector('.gameButton')
+    button: document.querySelector('.gameButton'),
+    result:document.querySelector('.result')
 }
 
+//etat lors de la partie
 var game = {
     state: false,
     input: {
@@ -25,8 +37,13 @@ var game = {
         R: false
     },
     time: 0,
-    note: []
-
+    note: [],
+    result:{
+        perfect:0,
+        cool:0,
+        bad:0,
+        miss:0
+    }
 };
 
 var interval;
@@ -37,12 +54,11 @@ gameElement.button.addEventListener('click', function (event) {
     startStop();
 })
 
+//inputs
 window.addEventListener('keydown', function (event) {
-
     if (event.which === 13) {
         startStop();
     }
-
     if (event.which === 65) {
         gameElement.letter[0].classList.add("active")
         game.input.A = true
@@ -56,13 +72,11 @@ window.addEventListener('keydown', function (event) {
     if (event.which === 69) {
         gameElement.letter[2].classList.add("active")
         game.input.E = true
-
     }
 
     if (event.which === 82) {
         gameElement.letter[3].classList.add("active")
         game.input.R = true
-
     }
 
     if (event.which === 32) {
@@ -70,6 +84,31 @@ window.addEventListener('keydown', function (event) {
     }
 })
 
+
+//relachement des inputs AZER
+window.addEventListener('keyup', function (event) {
+    if (event.which === 65) {
+        gameElement.letter[0].classList.remove("active")
+        game.input.A = false
+    }
+
+    if (event.which === 90) {
+        gameElement.letter[1].classList.remove("active")
+        game.input.Z = false
+    }
+
+    if (event.which === 69) {
+        gameElement.letter[2].classList.remove("active")
+        game.input.E = false
+    }
+
+    if (event.which === 82) {
+        gameElement.letter[3].classList.remove("active")
+        game.input.R = false
+    }
+})
+
+//les modifications de l'affichage et des éléments du jeu lorsque STOP ou START
 function startStop() {
     if (game.state === false) {
         reinit();
@@ -79,15 +118,17 @@ function startStop() {
         clearInterval(interval);
         interval = setInterval(function () {
             game.time += TT;
-            for(var i=0; i<50 ;i++) {
-                if (sequence.length > 0 && sequence[i] && sequence[i].timing - game.time < 10000) {
+            for(var i=0; i<10 ;i++) {
+                if (sequence.length > 0 && sequence[i] && sequence[i].timing - game.time < 2000) {
                     noteShow(sequence[i].timing - game.time);
+                    
                 }
             }
             clear();
             timePass();
             render();
             purge();
+            theEnd()
         }, TT);
     } else if (game.state === true) {
         gameElement.button.textContent = "Start"
@@ -102,32 +143,10 @@ function startStop() {
     }
 }
 
-window.addEventListener('keyup', function (event) {
-    if (event.which === 65) {
-        gameElement.letter[0].classList.remove("active")
-        game.input.A = false
-
-    }
-
-    if (event.which === 90) {
-        gameElement.letter[1].classList.remove("active")
-        game.input.Z = false
-    }
-
-    if (event.which === 69) {
-        gameElement.letter[2].classList.remove("active")
-        game.input.E = false
-
-    }
-
-    if (event.which === 82) {
-        gameElement.letter[3].classList.remove("active")
-        game.input.R = false
-    }
-})
-
-
+//push les notes de la sequence vers le tableau des notes à afficher dans les colonnes
 function noteShow(timeLeft) {
+    console.log(timeLeft);
+
     game.note.push({
         timeLeft: timeLeft,
         letter: sequence[0].letter
@@ -135,6 +154,7 @@ function noteShow(timeLeft) {
     sequence.splice(0, 1);
 }
 
+//diminue le temps restant des notes avant d'arriver en bas de la colonne
 function timePass() {
     if (game.note.length > 0) {
         for (var i = 0; i < game.note.length; i++) {
@@ -143,8 +163,8 @@ function timePass() {
     }
 }
 
+//affiche les notes à intervalle régulier
 function render() {
-
     if (game.note.length > 0) {
         for (var i = 0; i < game.note.length; i++) {
             var j = 0;
@@ -152,7 +172,7 @@ function render() {
                 if (game.note[i].letter[key] === true) {
                     var newP = document.createElement('p');
                     newP.classList.add('note');
-                    newP.style.bottom = game.note[i].timeLeft /10 + "%";
+                    newP.style.bottom = game.note[i].timeLeft /20 + "%";
                     gameElement.colonne[j].appendChild(newP);
                 }
                 j++;
@@ -162,6 +182,7 @@ function render() {
     }
 }
 
+//efface toute les notes avant le render
 function clear() {
     gameElement.colonne.forEach(element => {
         element.innerHTML = "";
@@ -178,9 +199,12 @@ function purge() {
         gameElement.container.classList.remove("cool");
         gameElement.container.classList.remove("bad");
         barChange("miss")
+        game.result.miss++
+        
     }
     }
 }
+
 
 
 function inputCheck() {
@@ -197,7 +221,7 @@ function inputCheck() {
             gameElement.container.classList.remove("bad");
             gameElement.container.classList.remove("miss");
             barChange("perfect")
-
+            game.result.perfect++
 
         } else if (check === 4 & (game.note[0].timeLeft < 100) && (game.note[0].timeLeft > -100)) {
             console.log('cool!!');
@@ -205,6 +229,7 @@ function inputCheck() {
             gameElement.container.classList.remove("bad");
             gameElement.container.classList.remove("miss");
             barChange("cool")
+            game.result.cool++
 
 
         } else {
@@ -213,6 +238,7 @@ function inputCheck() {
             gameElement.container.classList.remove("cool");
             gameElement.container.classList.remove("miss");
             barChange("bad")
+            game.result.bad++
 
         }
         game.note.splice(0, 1);
@@ -222,10 +248,41 @@ function inputCheck() {
 }
 
 function barChange(myClass) {
-
     gameElement.container.classList.add(myClass);
     clearTimeout(barTimeOut);
     barTimeOut = setTimeout(function () {
         gameElement.container.classList.remove(myClass);
     }, 400)
+}
+
+
+function theEnd(){
+    if(gameElement.player.currentTime===gameElement.player.duration){
+        gameElement.button.textContent = "Start"
+        gameElement.player.pause();
+        gameElement.player.currentTime = 0;
+        showResult();
+        game.state = false;
+        clearInterval(interval);
+        clear();
+        game.time = 0;
+        game.note = []
+    }
+}
+
+function showResult(){
+
+    gameElement.result.innerHTML="";
+    var newText = document.createElement('p');
+    newText.textContent = "PERFECT:" + game.result.perfect;
+    gameElement.result.appendChild(newText);
+    var newText = document.createElement('p');
+    newText.textContent = "COOL:" + game.result.cool;
+    gameElement.result.appendChild(newText);
+    var newText = document.createElement('p');
+    newText.textContent = "BAD:" + game.result.bad;
+    gameElement.result.appendChild(newText);
+    var newText = document.createElement('p');
+    newText.textContent = "MISS:" + game.result.miss;
+    gameElement.result.appendChild(newText);
 }
